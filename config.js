4b4df0a9a56546f0c5e30095fa9da7f903f94f3f
mark-laptop <markshapiro12345@gmail.com>
2016-07-05 14:35:39 +0300

    max results more

--- config.js
+++ config.js
@@ -3,7 +3,7 @@
 var _ = require('lodash');
 
 module.exports = {
-    maxResults: 10000,
+    maxResults: 500000,
     minDate: '2014-04-01',
     maxDecimalPlaces: 5,
     apiName:"counters/pub/getCounters"
